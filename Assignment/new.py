79e6ec2ed85c65eda052eaeed4cf7aa8b4eb05be
mohammedanaz <mohammedanaz@gmail.com>
2024-02-10 20:17:05 +0530

    m

--- Assignment/new.py
+++ Assignment/new.py
@@ -1,1 +1,7 @@
-print("new")
+name = input("\n\nEnter your name:")
+
+my_set = {"Good", "Bad", "Some times Bad","Some times Good", "Naughty"}
+
+value = my_set.pop()
+
+print(f"\n\n{name} is - {value}\n\n")
