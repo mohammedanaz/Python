6e60eba8675d05c6d48b922b7921d2bb975d469f
mohammedanaz <mohammedanaz@gmail.com>
2024-02-15 09:47:31 +0530

    m

--- PythonTest.py
+++ PythonTest.py
@@ -1,7 +1,9 @@
-import json
+def my_gen(n):
+    a, b = 0, 1
+    yield a
+    for i in range(n-1):
+        yield b
+        a, b = b, a + b
 
-json_str = '{"name": "Anaz", "Age": 40, "Loc": "clt"}'
-py_dict = json.loads(json_str)
-
-print(py_dict)
-
+for num in my_gen(2):
+    print(num, end=' ')
