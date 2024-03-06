cfd2a65ef6c956e4da9bc75fb4646015586a9900
mohammedanaz <mohammedanaz@gmail.com>
2024-03-06 18:54:23 +0530

    updates

--- PythonTest.py
+++ PythonTest.py
@@ -1,9 +1,5 @@
-def my_gen(n):
-    a, b = 0, 1
-    yield a
-    for i in range(n-1):
-        yield b
-        a, b = b, a + b
-
-for num in my_gen(2):
-    print(num, end=' ')
+# Reduce
+from functools import reduce
+numbers = [1, 2, 3, 4, 5]
+product = reduce(lambda x, y: x * y, numbers)
+print(product)  # Output: 120
