469e1aa21ead2d6cd1f8cfa72fa1213354a42013
mohammedanaz <mohammedanaz@gmail.com>
2024-04-08 16:09:47 +0530

    FibanocciRecursion added

--- PythonTest.py
+++
@@ -1,9 +0,0 @@
-def rcs(st):
-    if len(st) <= 1:
-        return True
-    elif st[0].lower() != st[-1].lower():
-        return False
-    else:
-        return rcs(st[1:-1])
-
-print(rcs('malaxdyalam'))
