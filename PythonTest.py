125e7e4413ef8afd18145ee5663aa320c73a82c3
mohammedanaz <mohammedanaz@gmail.com>
2024-07-26 09:18:00 +0530

    updates

--- PythonTest.py
+++ PythonTest.py
@@ -1,6 +1,5 @@
-def myFn(arr):
-    result = [x for x in arr if ord(x.lower()) >= 97 and ord(x.lower()) <= 122]
-    print(result)
+arr = [1,2,3,4,5]
 
-arr = ['H', 'h', '@', '/', 'l', 'l', '#', 'o']
-myFn(arr)
+st = ''.join(arr)
+
+print(st)
