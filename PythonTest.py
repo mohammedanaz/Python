456fa68425cc0e424f76c0f1c86d6e0a53da635e
mohammedanaz <mohammedanaz@gmail.com>
2024-07-26 11:04:55 +0530

    updates

--- PythonTest.py
+++ PythonTest.py
@@ -1,5 +1,13 @@
-arr = [1,2,3,4,5]
+arr = [1,2,3,4,5,1,2,6,7,3,9]
 
-st = ''.join(arr)
+st = 'test'
 
-print(st)
+def remove_dupe(arr):
+    new_lst = []
+    for x in arr:
+        if x not in new_lst:
+            new_lst.append(x)
+    return new_lst
+
+print(remove_dupe(arr))
+print(len(st))
