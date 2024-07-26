207d15768a01b03b852dc9c582ad9b3af350f209
mohammedanaz <mohammedanaz@gmail.com>
2024-07-26 14:31:06 +0530

    updates

--- PythonTest.py
+++ PythonTest.py
@@ -3,14 +3,6 @@
 arr = [1,2,3,4,5,1,2,6,7,3,9]
 
 st = 'test'
+st += 'new'
 
-def remove_dupe(arr):
-    new_lst = []
-    for x in arr:
-        if x not in new_lst:
-            new_lst.append(x)
-    return new_lst
-
-
-print(remove_dupe(arr))
-print(st)
+print(arr.count(5))
