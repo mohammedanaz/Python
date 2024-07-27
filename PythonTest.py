12d0914e7223d7c5189e2906973511ac3c0dd729
mohammedanaz <mohammedanaz@gmail.com>
2024-07-27 20:29:07 +0530

    updates

--- PythonTest.py
+++ PythonTest.py
@@ -1,20 +1,9 @@
-
-class MyException(Exception):
+def my_fn(*args, **kwargs):
     
-    def __init__(self, msg):
-        self.msg = f'My Exception Caught: {msg}'
+    value = kwargs.get('name')
+    if value:
+        print(value)
+    else:
+        print('no such key')
 
-def my_fn(a, b):
-
-    if a/b < 1:
-        raise MyException('Divisor cant be less than dividend')
-    
-    return a/b
-try:
-    print(my_fn(2, 3))
-except MyException as e:
-    print(str(e))
-
-x = 2
-
-print(x)
+my_fn(temp='Adam')
