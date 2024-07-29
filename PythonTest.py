0c562507084fcb3c073a5d788adc0e4ea1756855
mohammedanaz <mohammedanaz@gmail.com>
2024-07-29 12:54:05 +0530

    re charset test

--- PythonTest.py
+++ PythonTest.py
@@ -6,6 +6,12 @@ result = pattern.search(txt)
 
 print(result.string,'\n', result.span(),'\n', result.start(),'\n', result.group())
 
-sub = re.sub(r'\bs.*e\b', 'newWord', txt)
+sub = re.sub(r' ', '-', txt)
 
 print(sub)
+
+txt1 = 'Adam is here'
+
+result1 = re.search(r'\bA[a,d,m]{3}', txt1)
+
+print(result1.group())
