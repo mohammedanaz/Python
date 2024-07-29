4f3b43855b285e19eb14ca972c1ec1812f592adb
mohammedanaz <mohammedanaz@gmail.com>
2024-07-29 14:42:40 +0530

    traceback test

--- PythonTest.py
+++ PythonTest.py
@@ -1,21 +1,7 @@
-import re
-txt = 'This is a sample text to test regex.'
-pattern = re.compile(r'\bsa')
+import traceback
 
-result = pattern.search(txt)
-
-print(result.string,'\n', result.span(),'\n', result.start(),'\n', result.group())
-
-sub = re.sub(r' ', '-', txt)
-
-print(sub)
-
-txt1 = 'this is a sample text. this contains this.'
-
-pattern = r'(\w+)i(\w+)'
-grp1 = re.findall(pattern, txt1)
-
-print(grp1)
-
-first, last = grp1[0]
-print(first, last)
+try:
+    x = 1/0
+except ZeroDivisionError:
+    # traceback.print_exc()
+    print('error')
