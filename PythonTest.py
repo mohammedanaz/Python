152732d3ad08be9bf103ff1b6ed49f5889d1ac3f
mohammedanaz <mohammedanaz@gmail.com>
2024-08-05 12:12:43 +0530

    done

--- PythonTest.py
+++ PythonTest.py
@@ -3,5 +3,5 @@ import re
 with open('SampleText.txt', 'r+') as f:
     content = f.read()
     f.seek(0)
-    new_content = re.sub(r'^a', 'A', re.MULTILINE)
+    new_content = re.sub(r'^a', 'A', content)
     f.write(new_content)
