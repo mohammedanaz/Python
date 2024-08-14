fe2ad7cd397e2404d186b7c990d1200cc7a718b8
mohammedanaz <mohammedanaz@gmail.com>
2024-08-14 09:25:26 +0530

    practice updates

--- PythonTest.py
+++ PythonTest.py
@@ -1,25 +1,3 @@
-
-def my_decor(func):
-    def inner(*args, **kwargs):
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         
-        print(args, kwargs)
-        a = func(*args, **kwargs)
-        print(a)
-
-    return inner
-        
-
-
-
-@my_decor
-def div(a, b):
-    return a / b
-@my_decor
-def cap(st):
-    return st.upper()
-
-div(b=2, a=4)
-cap('adam')
-
-
-
