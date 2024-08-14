43c020c58380d1c9b0858492b3bce440f51990fd
mohammedanaz <mohammedanaz@gmail.com>
2024-08-14 09:44:42 +0530

    time limit exeeded

--- PythonTest.py
+++ PythonTest.py
@@ -29,4 +29,4 @@ class Solution:
             for j in range(i+1, len(prices)):
                 if (prices[j] - prices[i]) > max_profit:
                     max_profit = prices[j] - prices[i]
-            return max_profit
+        return max_profit
