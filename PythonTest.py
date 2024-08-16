b78826d1a02bb0fb3ec5edccdc61cd47a4fb699c
mohammedanaz <mohammedanaz@gmail.com>
2024-08-16 09:01:01 +0530

    updates

--- PythonTest.py
+++ PythonTest.py
@@ -1,2 +1,16 @@
 
 # find longest substring in a string without repeating chars
+
+s = 'abcabcdabc'
+
+result = 0
+sub_sting_arr = []
+
+for x in s:
+    if x not in sub_sting_arr:
+        sub_sting_arr.append(x)
+    else:
+        if len(sub_sting_arr) > result:
+            result = len(sub_sting_arr)
+        sub_sting_arr.clear()
+        sub_sting_arr.append(x)
