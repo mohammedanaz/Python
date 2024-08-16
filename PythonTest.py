77d387f9f14d0318829412e5872acb2168885e39
mohammedanaz <mohammedanaz@gmail.com>
2024-08-16 09:14:07 +0530

    flag removed

--- PythonTest.py
+++ PythonTest.py
@@ -5,18 +5,17 @@ s = 'abcabcdabc'
 
 result = 0
 sub_sting_arr = []
-flag = 0
 
 for x in s:
     if x not in sub_sting_arr:
         sub_sting_arr.append(x)
+        if len(sub_sting_arr) > result:
+            result = len(sub_sting_arr)
     else:
         flag = 1
         if len(sub_sting_arr) > result:
             result = len(sub_sting_arr)
         sub_sting_arr.clear()
         sub_sting_arr.append(x)
-if flag == 0:
-    result = len(sub_sting_arr)
 
 
