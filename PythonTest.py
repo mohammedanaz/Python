898980ec05b505e5c879850bdcaa123c97749f14
mohammedanaz <mohammedanaz@gmail.com>
2024-08-28 10:49:14 +0530

    concurrent futures module

--- PythonTest.py
+++ PythonTest.py
@@ -8,8 +8,9 @@ def todo(seconds):
     return 'sleep done'
 
 with concurrent.futures.ThreadPoolExecutor() as ex:
-    f= ex.submit(todo, 1)
-    print(f.result())
+    results = [ex.submit(todo, 1) for _ in range(10)]
+    for f in results:
+        print(f.result())
 
 end = time.perf_counter()
 time_gap = round(end-start, 2)
