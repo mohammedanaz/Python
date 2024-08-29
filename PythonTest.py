d4ea75bab381cc6ef662ec78985a6746deb8513e
mohammedanaz <mohammedanaz@gmail.com>
2024-08-29 17:02:52 +0530

    test c

--- PythonTest.py
+++ PythonTest.py
@@ -2,4 +2,12 @@
 import asyncio
 
 async def myfn():
-    await # long code
+    await # long code A
+
+    # other codes B
+
+async def myfn():
+    task = asyncio.create_task(# long code A) 
+
+    # other codes B
+
