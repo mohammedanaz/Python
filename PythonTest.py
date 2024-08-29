35dc937bf2d3ba78e7d0b1aaaf46dd562dc3e827
mohammedanaz <mohammedanaz@gmail.com>
2024-08-29 17:01:01 +0530

    test a

--- PythonTest.py
+++ PythonTest.py
@@ -1,19 +1,5 @@
-import concurrent.futures
-import time
 
-start = time.perf_counter()
-def todo(seconds):
-    print(f'sleeping for {seconds} seconds')
-    time.sleep(seconds)
-    return f'sleep done...in {seconds} seconds'
+import asyncio
 
-with concurrent.futures.ThreadPoolExecutor() as ex:
-    secs = [5,4,3,2,1]
-    results = [ex.submit(todo, sec) for sec in secs]
-    for f in concurrent.futures.as_completed(results):
-        print(f.result())
-
-end = time.perf_counter()
-time_gap = round(end-start, 2)
-
-print(f'Finished in {time_gap} seconds.')
+async def myfn():
+    await # long code
