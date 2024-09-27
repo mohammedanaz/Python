a6161857eb4614307c685a0e23dbad1e7099d188
mohammedanaz <mohammedanaz@gmail.com>
2024-09-27 09:23:16 +0530

    temp

--- PythonTest.py
+++ PythonTest.py
@@ -1,29 +1,12 @@
-arr1 = [1,5,10,20,40,80]
-arr2 = [6,7,20,80,100]
-arr3 = [3,4,15,20,30,70,80,120]
+x =-1
 
-result = []
+def change(a):
+    print('a-',a)
+    return abs(a)
+print('x-', x)
+b = change(x)
 
-for x in arr3:
-    if x in arr2 and x in arr1:
-        result.append(x)
+print('b -', b)
+# x =b
 
-print(result)
-
-# i,j,k = 0,0,0
-# result = []
-
-# while i<len(arr1) and j<len(arr2) and k<len(arr3):
-#     if arr1[i] == arr2[j] == arr3[k]:
-#         result.append(arr1[i])
-#         i+=1
-#         j+=1
-#         k+=1
-#     elif arr1[i] < arr2[j]:
-#         i+=1
-#     elif arr2[j] < arr3[k]:
-#         j+=1
-#     else:
-#         k+=1
-
-# print(result)
+print('x -', x)
