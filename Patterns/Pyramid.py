29caf7189044d13f63f09dc4e28b39e56e1145ac
mohammedanaz <mohammedanaz@gmail.com>
2024-12-28 10:38:10 +0530

    pyramid

--- Patterns/Pyramid.py
+++ Patterns/Pyramid.py
@@ -1,4 +1,9 @@
 
 for i in range(6):
     for j in range(10):
-        
+        if j<= 5-i or j > 4 + i:
+            print(' ', end='')
+        else:
+            print('*', end='')
+
+    print('')
